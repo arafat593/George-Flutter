470b09fd79afb7a9bc1ad697cb48ff9c7c59d354
Md. Al Arafat <arafathossain593@gmail.com>
2026-02-24 10:55:27 +0600

    management screen builder change

--- lib/management.dart
+++ lib/management.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:george/app/modules/error_screen/views/error_screen_view.dart';
+import 'package:george/app/modules/not_found_screen/bindings/not_found_screen_binding.dart';
+import 'package:george/app/modules/not_found_screen/views/not_found_screen_view.dart';
 import 'package:get/get.dart';
 import 'app/data/theme.dart';
 import 'app/routes/app_pages.dart';
@@ -9,15 +12,19 @@ class Management extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AppSize.size = MediaQuery.of(context).size;
     return GetMaterialApp(
       title: "Application",
       theme: themeData(),
       debugShowCheckedModeBanner: false,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      unknownRoute: GetPage(name: Routes.NOT_FOUND_SCREEN, page: () => const NotFoundScreenView(), binding: NotFoundScreenBinding()),
       builder: (context, child) {
-        AppSize.size = MediaQuery.of(context).size;
-        return child!;
+        ErrorWidget.builder = (FlutterErrorDetails errorDetails) {
+          return Overlay(initialEntries: [OverlayEntry(builder: (context) => ErrorScreenView())]);
+        };
+        return Overlay(initialEntries: [OverlayEntry(builder: (context) => child ?? SizedBox())]);
       },
     );
   }
